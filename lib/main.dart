51af2774da3dc1a269ca59317373c39657fb6105
Sothiry In <insothiry@gmail.com>
2024-05-17 08:50:16 +0700

    uncomment main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,29 @@
-// import 'package:api_dio/api_call.dart';
-// import 'package:flutter/material.dart';
+import 'package:api_dio/api_call.dart';
+import 'package:flutter/material.dart';
 
-// void main() {
-//   runApp(const MyApp());
-// }
+void main() {
+  runApp(const MyApp());
+}
 
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Flutter Demo',
-//       theme: ThemeData(
-//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-//         useMaterial3: true,
-//       ),
-//       home: Scaffold(
-//         appBar: AppBar(
-//           title: Text('API Development'),
-//           elevation: 5,
-//         ),
-//         body: ApiCall(),
-        
-//       ),
-//     );
-//   }
-// }
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('API Development'),
+          elevation: 5,
+        ),
+        body: ApiCall(),
+      ),
+    );
+  }
+}
